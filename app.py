a423a1dabdfda3d454b460565032a929f0bd1f33
TgrRys <tegarrizky237@gmail.com>
2023-10-06 21:31:48 +0700

    add analysis word cloud by comment & limit data

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ import instaloader
 from os.path import expanduser
 import pandas as pd
 import datetime
-from flask import Flask, jsonify, request, render_template
-from io import StringIO
-# from flask_cors import CORS
+from flask import Flask, jsonify, request, render_template, send_file
+from io import StringIO, BytesIO
+from textblob import TextBlob
+from wordcloud import WordCloud
+import matplotlib
+matplotlib.use('Agg')  # Use the 'Agg' backend for non-interactive use
+import matplotlib.pyplot as plt
+import networkx as nx
+from networkx.drawing.nx_pydot import graphviz_layout
+import nxpd
 
 # Create an instance of Instaloader
 loader = instaloader.Instaloader()
@@ -13,34 +20,35 @@ loader = instaloader.Instaloader()
 app = Flask(__name__)
 
 # Set the template folder path to your desired location
-app = Flask(__name__, template_folder="C:/xampp/htdocs/Scraper")
+app.template_folder = expanduser("D:/xampp/htdocs/Senitiment-Analysis-Instagram/")
 
 @app.route('/')
 def index():
     # Render the HTML template for the main page
     return render_template('index.php')
 
-
-
-
 @app.route('/get-instagram-profile', methods=['GET'])
 def get_instagram_profile():
     try:
-        # Get the profile username from the query parameters
+        # Get the profile username and limit from the query parameters
         username = request.args.get('username')
+        limit = int(request.args.get('limit')) if 'limit' in request.args else None
 
         # Retrieve the public profile
         profile = instaloader.Profile.from_username(loader.context, username)
 
+        # Get the follower count for the profile
         follower_count = profile.followers
-        
 
         # Initialize a list to store post data as dictionaries
         profile_data = []
 
+        # Initialize a list to store all captions for word cloud generation
+        all_captions = []
+
         # Iterate over the profile's posts
         for post in profile.get_posts():
-            print(follower_count)
+            # Initialize post_data dictionary for each post
             post_data = {
                 "date": post.date_local.strftime('%Y-%m-%d'),
                 "caption": post.caption,
@@ -48,17 +56,62 @@ def get_instagram_profile():
                 "comments": post.comments,
                 "engagement": int(post.comments) + int(post.likes),
                 "followers": follower_count,
-                "link": f'<a href="https://www.instagram.com/p/{post.shortcode}/">Link Postingan</a>'
+                "link": f'https://www.instagram.com/p/{post.shortcode}/',
+                "shortcode": f'https://www.instagram.com/p/{post.shortcode}/',
+                "sentimen": "Netral"  # Inisialisasi sentimen ke "Netral"
             }
-            print(post_data)
-            profile_data.append(post_data)
+
+            # Append the caption to the list for word cloud
+            caption = post.caption
+            if caption:
+                all_captions.append(caption)
+
+            # Analisis sentimen dengan TextBlob
+            if caption:
+                analysis = TextBlob(caption)
+                sentiment = analysis.sentiment
+                # Dapatkan nilai sentimen (range dari -1 hingga 1)
+                sentiment_value = sentiment.polarity
+                # Beri label sentimen berdasarkan nilai sentimen
+                if sentiment_value > 0:
+                    post_data["sentimen"] = "Positif"
+                elif sentiment_value < 0:
+                    post_data["sentimen"] = "Negatif"
+                else:
+                    post_data["sentimen"] = "Netral"
+
+            # Tambahkan data postingan ke dalam profile_data jika belum mencapai limit
+            if limit is None or len(profile_data) < limit:
+                profile_data.append(post_data)
+            else:
+                break  # Hentikan iterasi jika sudah mencapai limit
+
+        # Generate and save the word cloud to a file
+        if all_captions:
+            wordcloud = WordCloud(width=800, height=400).generate(' '.join(all_captions))
+            fig, ax = plt.subplots(figsize=(10, 5))
+            ax.imshow(wordcloud, interpolation='bilinear')
+            ax.axis("off")
+            plt.savefig('wordcloud.png', format='png', bbox_inches='tight')
+            plt.close()
 
         # Create a response with CORS headers
         response = jsonify(profile_data)
         response.headers.add('Access-Control-Allow-Origin', '*')
 
         return response, 200
-        
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/get-word-cloud', methods=['GET'])
+def get_word_cloud():
+    try:
+        # Read the word cloud image from the file
+        with open('wordcloud.png', 'rb') as f:
+            image = BytesIO(f.read())
+
+        return send_file(image, mimetype='image/png')
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
--- index.php
+++ index.php
@@ -1,91 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Instagram Profile Posts Data</title>
-    <!-- Tambahkan Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        /* Styling utama */
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
-            margin: 0;
-            padding: 0;
-        }
-
-        h1 {
-            text-align: center;
-            margin: 20px 0;
-            color: #333;
-            /* Ubah warna judul */
-        }
-
-        /* Gaya input dan tombol */
-        .form-group {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-
-        .input-group {
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            justify-content: center;
-        }
-
-        #username {
-            flex-grow: 1;
-        }
-
-        #fetch-button {
-            margin-left: 10px;
-        }
-
-        /* Gaya tabel */
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            background-color: white;
-            margin-top: 20px;
-            text-align: center;
-        }
-
-        /* Gaya tautan */
-        .post-link {
-            color: #007bff;
-            text-decoration: none;
-        }
-
-        .post-link:hover {
-            text-decoration: underline;
-        }
-    </style>
+    <title>Instagram Profile Post Data</title>
+    <!-- Include Bootstrap CSS from CDN -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
 <body>
-    <h1>Instagram Profile Posts Data</h1>
     <div class="container">
-        <form class="form-group">
-            <div class="input-group">
-                <label for="username" class="col-sm-3 col-form-label">Enter Instagram Username:</label>
-                <div class="col-sm-6">
+        <h1 class="text-center mt-5">Instagram Profile Posts Data</h1>
+        <form class="d-flex flex-column align-items-center mt-4">
+            <div class="form-group w-100">
+                <label for="username">Enter Instagram Username:</label>
+                <div class="input-group">
                     <input type="text" class="form-control" id="username" name="username" required>
+                    <div class="input-group-append">
+                        <button type="button" class="btn btn-dark" id="fetch-button">Fetch Data</button>
+                    </div>
                 </div>
-                <div class="col-sm-3">
-                    <button type="button" class="btn btn-success" id="fetch-button">Fetch Data</button>
-                </div>
+            </div>
+            <!-- Add an input for limiting the number of posts -->
+            <div class="form-group w-100">
+                <label for="limit">Limit the number of posts:</label>
+                <input type="number" class="form-control" id="limit" name="limit">
             </div>
         </form>
 
         <!-- Display the fetched data here -->
-        <div id="profile-data">
+        <div id="profile-data" class="mt-5">
             <!-- Table to display the fetched data -->
-            <table class="table table-bordered" id="post-table">
-                <thead>
+            <table class="table table-bordered table-hover">
+                <thead class="thead-dark">
                     <tr>
                         <th>Post Date</th>
                         <th>Caption</th>
@@ -93,33 +39,37 @@
                         <th>Likes</th>
                         <th>Comments</th>
                         <th>Followers</th>
-                        <th>Engagement</th>
-                        <th>Sentiment</th>
+                        <th>Average</th>
+                        <th>Sentimen</th>
                     </tr>
                 </thead>
-                <tbody>
+                <!-- Give an id to tbody -->
+                <tbody id="post-table-body">
                     <!-- Posts will be added here dynamically -->
                 </tbody>
             </table>
         </div>
-    </div>
 
-    <!-- Tambahkan Bootstrap JavaScript dan jQuery (diperlukan oleh Bootstrap) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <!-- Display the word cloud container -->
+        <div id="word-cloud" class="mt-5">
+            <h2 class="text-center">Word Cloud</h2>
+            <div id="word-cloud-canvas" class="d-flex justify-content-center"></div>
+        </div>
+        
+    </div>
 
     <script>
         // Function to make an AJAX request and update the table
         function fetchProfileData() {
             const username = document.getElementById('username').value;
-            const url = `http://127.0.0.1:5000/get-instagram-profile?username=${username}`;
+            const limit = document.getElementById('limit').value; // Get the limit input
+            const url = `/get-instagram-profile?username=${username}&limit=${limit}`;
 
             fetch(url)
                 .then(response => response.json())
                 .then(data => {
                     // Create a table row for each post
-                    const tableBody = document.querySelector('#post-table tbody');
+                    const tableBody = document.querySelector('#post-table-body'); // Select the tbody with id
                     tableBody.innerHTML = '';
 
                     data.forEach(post => {
@@ -127,7 +77,7 @@
                         row.innerHTML = `
                             <td>${post.date}</td>
                             <td>${post.caption}</td>
-                            <td><a class="post-link">${post.link}</a></td>
+                            <td><a href="${post.link}" target="_blank">Link Postingan</a></td>
                             <td>${post.likes}</td>
                             <td>${post.comments}</td>
                             <td>${post.followers}</td>
@@ -142,37 +92,38 @@
                 });
         }
 
-        // Function to generate and trigger the CSV download
-        function downloadCSV(data) {
-            const csv = 'Post Date,Caption,Likes,Comments,Engagement\n' + data.map(post => (
-                `${post.date},"${post.caption}",${post.likes},${post.comments},${post.engagement}`
-            )).join('\n');
+        // Function to display the word cloud
+        function displayWordCloud() {
+            const wordCloudCanvas = document.getElementById('word-cloud-canvas');
+            const wordCloudImage = new Image();
 
-            const blob = new Blob([csv], {
-                type: 'text/csv'
-            });
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = 'profile_data.csv';
-            document.body.appendChild(a);
-            a.click();
-            window.URL.revokeObjectURL(url);
+            // Generate a timestamp to append to the image URL
+            const timestamp = Date.now();
+
+            // Fetch the word cloud image from the server with the timestamp
+            fetch(`/get-word-cloud?timestamp=${timestamp}`)
+                .then(response => response.blob())
+                .then(blob => {
+                    const url = URL.createObjectURL(blob);
+                    wordCloudImage.src = url;
+                    wordCloudCanvas.innerHTML = ''; // Clear previous image
+                    wordCloudCanvas.appendChild(wordCloudImage);
+                })
+                .catch(error => {
+                    console.error('Error fetching word cloud:', error);
+                });
         }
 
         // Add click event listener to the "Fetch Data" button
-        document.getElementById('fetch-button').addEventListener('click', fetchProfileData);
-
-        // Add click event listener to the "Download CSV" button
-        document.getElementById('download-button').addEventListener('click', () => {
-            fetch(`http://127.0.0.1:5000/get-instagram-profile?username=${document.getElementById('username').value}`)
-                .then(response => response.json())
-                .then(data => downloadCSV(data))
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
+        document.getElementById('fetch-button').addEventListener('click', () => {
+            fetchProfileData();
+            displayWordCloud(); // Display the word cloud when fetching data
         });
     </script>
-</body>
 
+    <!-- Include Bootstrap JS and jQuery from CDN -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
 </html>
